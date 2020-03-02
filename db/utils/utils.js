365b2a89e1a6f5fc25de69f1404be7077010423f
vanessa <20596550+wordlesspirate@users.noreply.github.com>
2020-03-02 15:11:07 +0000

    added tests and functionality for formatDate() util function

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,9 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  const convertedDate = list.map(({ created_at, ...otherProperties }) => {
+    return { created_at: new Date(created_at), ...otherProperties };
+  });
+  return convertedDate;
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,101 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+  formatComments
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates()", () => {
+  it("If no input is provided, returns an empty array.", () => {
+    const input = [];
+    const output = [];
+    expect(formatDates(input)).to.eql(output);
+  });
+  it("For an array containing only one object, converts timestamp to date.", () => {
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const output = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: new Date(1542284514171),
+        votes: 100
+      }
+    ];
+    expect(formatDates(input)).to.eql(output);
+  });
+  it("For an array containing several objects, converts timestamps to date.", () => {
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "some gifs",
+        created_at: 1289996514171
+      }
+    ];
+    const output = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: new Date(1542284514171),
+        votes: 100
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "some gifs",
+        created_at: new Date(1289996514171)
+      }
+    ];
+    expect(formatDates(input)).to.eql(output);
+  });
+  it("Original input has not been mutated.", () => {
+    const input = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    formatDates(input);
+    expect(input).to.eql([
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ]);
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
