02a877e506de322a7c0d2224e07effbbeb9fa29d
vanessa <20596550+wordlesspirate@users.noreply.github.com>
2020-03-02 16:18:26 +0000

    removed superfluous elements from articles and comments migrations tables

--- db/migrations/20200302123321_create_table_articles.js
+++ db/migrations/20200302123321_create_table_articles.js
@@ -1,11 +1,17 @@
 exports.up = function(knex) {
   return knex.schema.createTable("articles", articlesTable => {
-    articlesTable.increments("article_id").primary();
+    articlesTable.increments("article_id");
     articlesTable.string("title").notNullable();
     articlesTable.text("body").notNullable();
     articlesTable.integer("votes").defaultTo(0);
-    articlesTable.string("topic").references("topics.slug");
-    articlesTable.string("author").references("users.username");
+    articlesTable
+      .string("topic")
+      .references("topics.slug")
+      .notNullable();
+    articlesTable
+      .string("author")
+      .references("users.username")
+      .notNullable();
     articlesTable.timestamp("created_at").defaultTo(knex.fn.now());
   });
 };
--- db/migrations/20200302125542_create_table_comments.js
+++ db/migrations/20200302125542_create_table_comments.js
@@ -1,8 +1,14 @@
 exports.up = function(knex) {
   return knex.schema.createTable("comments", commentsTable => {
-    commentsTable.increments("comment_id").primary();
-    commentsTable.string("author").references("users.username");
-    commentsTable.integer("article_id").references("articles.article_id");
+    commentsTable.increments("comment_id");
+    commentsTable
+      .string("author")
+      .references("users.username")
+      .notNullable();
+    commentsTable
+      .integer("article_id")
+      .references("articles.article_id")
+      .notNullable();
     commentsTable.integer("votes").defaultTo(0);
     commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
     commentsTable.text("body").notNullable();
