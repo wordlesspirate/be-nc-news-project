77f13ab5a664f9fea44b0d77cc6f427eb6a2bdfc
vanessa <20596550+wordlesspirate@users.noreply.github.com>
2020-03-02 13:55:48 +0000

    added query-db.sql and corresponding text output to help clarify the result of migrations in nc_news_test

---
+++ db/query-db.sql
@@ -0,0 +1,12 @@
+\c nc_news_test
+
+\dt
+
+SELECT * FROM topics;
+SELECT COUNT(*) FROM topics;
+SELECT * FROM users;
+SELECT COUNT(*) FROM users;
+SELECT * FROM articles;
+SELECT COUNT(*) FROM articles;
+SELECT * FROM comments;
+SELECT COUNT(*) FROM comments;
