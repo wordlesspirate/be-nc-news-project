9334d152a11826f7128323961d4815b4ed4a4fb2
vanessa <20596550+wordlesspirate@users.noreply.github.com>
2020-05-28 15:39:10 +0100

    add details to query-db file

--- db/query-db.sql
+++ db/query-db.sql
@@ -25,3 +25,11 @@ SELECT COUNT(*) FROM comments;
 \d articles
 \d comments
 
+SELECT * FROM topics;
+SELECT COUNT(*) FROM topics;
+SELECT * FROM users;
+SELECT COUNT(*) FROM users;
+SELECT * FROM articles;
+SELECT COUNT(*) FROM articles;
+SELECT * FROM comments;
+SELECT COUNT(*) FROM comments;
