c620b9836491201f576b392cf9730fc9ea67fb8c
vanessa <20596550+wordlesspirate@users.noreply.github.com>
2020-05-28 15:36:49 +0100

    add route for /api path

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,15 @@
 const apiRouter = require("express").Router();
+
+const { getEndpoints } = require("../controllers/endpoints");
+
 const topicsRouter = require("./topics");
 const usersRouter = require("./users");
 const articlesRouter = require("./articles");
 const commentsRouter = require("./comments");
+
+const { invalidMethodsErrorHandler } = require("../errors");
+
+apiRouter.route("/").get(getEndpoints).all(invalidMethodsErrorHandler);
 
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
