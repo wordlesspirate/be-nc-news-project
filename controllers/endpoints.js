83427e04753b371154c9aadbdf923208fc6d1331
vanessa <20596550+wordlesspirate@users.noreply.github.com>
2020-05-29 11:27:08 +0100

    correction of typo on endpoints controller

--- controllers/endpoints.js
+++ controllers/endpoints.js
@@ -3,6 +3,7 @@ const { fetchEndpoints } = require("../models/endpoints.js");
 exports.getEndpoints = (req, res, next) => {
   fetchEndpoints()
     .then((endpoints) => {
+      // console.log(endpoints);
       res.status(200).send({ endpoints });
     })
     .catch(next);
