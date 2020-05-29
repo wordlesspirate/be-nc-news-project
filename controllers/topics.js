bdf10387c47898e00e90ea251f1b0920bcde5b27
vanessa <20596550+wordlesspirate@users.noreply.github.com>
2020-05-29 11:26:17 +0100

    changes to topics model and controller

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,30 +1,11 @@
 const { fetchTopics, checkTopicExists } = require("../models/topics");
 const { fetchAllArticles } = require("../models/articles");
 
-// exports.getTopics = (req, res, next) => {
-//   fetchTopics()
-//     .then(topics => {
-//       res.status(200).send({ topics });
-//     })
-//     .catch(next);
-// };
-
-//changes
-
 exports.getTopics = (req, res, next) => {
-  const { topic } = req.query;
-
-  if (topic) {
-    Promise.all([checkTopicExists(topic), fetchAllArticles(req.query)])
-      .then(([topic, articles]) => {
-        res.status(200).send({ articles });
-      })
-      .catch(next);
-  } else {
-    fetchTopics()
-      .then(topics => {
-        res.status(200).send({ topics });
-      })
-      .catch(next);
-  }
+  fetchTopics()
+    .then((topics) => {
+      // console.log(topics);
+      res.status(200).send({ topics });
+    })
+    .catch(next);
 };
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -8,11 +8,11 @@ chai.use(require("chai-sorted"));
 chai.use(require("sams-chai-sorted"));
 chai.use(require("chai-json"));
 
-describe("methods and routes in 'app' for NC News project", () => {
+describe("Tests for Nortcoders News API", () => {
   beforeEach(() => connection.seed.run());
   after(() => connection.destroy());
-  describe("Method: GET | Path: '/api' | Status: 200", () => {
-    it("responds with JSON file containing server's endpoints and supported queries", () => {
+  describe("'/api' | GET | 200", () => {
+    xit("responds with JSON file containing server's endpoints and supported queries", () => {
       return request(app)
         .get("/api")
         .expect(200)
@@ -21,8 +21,8 @@ describe("methods and routes in 'app' for NC News project", () => {
         });
     });
   });
-  describe("Method: GET | Path: '/apz' | Status: 404", () => {
-    it("responds with not found for any nonexistent path", () => {
+  describe("'/apz' | GET | 404", () => {
+    xit("responds with not found for any nonexistent path", () => {
       return request(app)
         .get("/apz")
         .expect(404)
@@ -31,12 +31,38 @@ describe("methods and routes in 'app' for NC News project", () => {
         });
     });
   });
-  describe("Method: INVALID METHODS (PATCH, PUT and DELETE) | Path: '/api' | Status: 405", () => {
-    it("sends invalid method error message if wrong method is used on specified path", () => {
-      const invalidMethods = ["patch", "put", "delete"];
+  describe("/api'| POST, PATCH, PUT and DELETE | 405", () => {
+    xit("responds with error if invalid method is used on specified path", () => {
+      const invalidMethods = ["post", "patch", "put", "delete"];
       const methodPromises = invalidMethods.map((method) => {
         return request(app)
           [method]("/api")
+          .expect(405)
+          .then(({ body: { msg } }) => {
+            expect(msg).to.equal("Sorry, method not allowed on this path!");
+          });
+      });
+      return Promise.all(methodPromises);
+    });
+  });
+  describe("'/api/topics' | GET | 200", () => {
+    it("responds with array of topic objects", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body: { topics } }) => {
+          expect(topics).to.be.an("array");
+          expect(topics).to.have.lengthOf(3);
+          expect(topics[0]).to.contain.keys("slug", "description");
+        });
+    });
+  });
+  describe("'/api/topics' | PATCH, PUT and DELETE | 405", () => {
+    it("responds with error if invalid method is used on specified path", () => {
+      const invalidMethods = ["patch", "put", "delete"];
+      const methodPromises = invalidMethods.map((method) => {
+        return request(app)
+          [method]("/api/topics")
           .expect(405)
           .then(({ body: { msg } }) => {
             expect(msg).to.equal("Sorry, method not allowed on this path!");
